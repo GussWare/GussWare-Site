896dfb70a4a6b431e76f19470c1394e75ce6a0c8
GussWare <gussware@gmail.com>
2026-09-10 18:10:22 -0600

    feat: re estructuración del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [sitemap()]
+  integrations: [sitemap()],
 });
